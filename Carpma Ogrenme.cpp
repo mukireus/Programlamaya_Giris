924bafb05a753804f3946d8f44a8e226f7feadce
Muhammet ÖMER <muhammet.omer@outlook.com>
2018-02-06 23:25:07 +0300

    Update Carpma Ogrenme.cpp

--- Carpma Ogrenme.cpp
+++ Carpma Ogrenme.cpp
@@ -8,21 +8,20 @@
 int main(){
     int a,b,c,d;
     srand(time(NULL));
-    for(a=1;a<=100;a++)
-	{
+    for(a=1;a<=100;a++){
     	b=rand()%10;
         c=rand()%10;
         printf("%d*%d= ",b,c);
         scanf("%d",&d);
-        if (d==b*c)
+		if (d==b*c)
 		{
-            printf("aferin, dogru... \n");
-        }
-        else
+		    printf("aferin, dogru... \n");
+		}
+		else
 		{
-        	printf("bilemedin...");
-        	break;
-        }
+			printf("bilemedin...");
+			break;
+		}
 	}
     getch();
 }
