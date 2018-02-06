c0e0b60afbb70ba0994a30dc4885e2efacc3dbcd
Muhammet ÖMER <muhammet.omer@outlook.com>
2018-02-06 21:15:55 +0300

    Add files via upload

---
+++ Carpma Ogrenme.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<time.h>
+int main(){
+    int a,b,c,d;
+    srand(time(NULL));
+    for(a=1;a<=100;a++)
+	{
+    	b=rand()%10;
+        c=rand()%10;
+        printf("%d*%d= ",b,c);
+        scanf("%d",&d);
+        if (d==b*c)
+		{
+            printf("aferin, dogru... \n");
+        }
+        else
+		{
+        	printf("bilemedin...");
+        	break;
+        }
+	}
+    getch();
+}
