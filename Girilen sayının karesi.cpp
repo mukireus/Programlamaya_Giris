2c7fc4728b8bfc928031959bf46a0c806370cc5f
Muhammet ÖMER <muhammet.omer@outlook.com>
2018-02-06 23:22:35 +0300

    Add files via upload

---
+++ Girilen sayının karesi.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int sayi,karesi;
+	cout << "Karesi alinicak sayiyi giriniz: ";
+	cin >> sayi;
+	karesi = sayi * sayi;
+	cout << "Girilen sayinin karesi: " << karesi;
+
+
+
+	return 0;
+}
+
