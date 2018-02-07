b3162bc1d0a5a141f927bebf8ee6ebe2ee6eb966
Muhammet ÖMER <muhammet.omer@outlook.com>
2018-02-08 00:14:08 +0300

    Add files via upload

---
+++ Ogretmenler Gunu Kutlama.cpp
@@ -0,0 +1,35 @@
+///////////////////////////////////////////
+// MUHAMMET ÖMER TARAFINDAN YAZILMIÞTIR ///
+///////////////////////////////////////////
+
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL,"Turkish");
+	cout << "			Kimden: " << endl;
+	cout << "			Kime  : 	" << endl;
+	cout << endl;
+	cout << "		";
+	for (int i =0; i<42; i++)
+		cout << "_";
+	cout <<endl <<endl;
+	cout << "				i i i i" << endl;
+	cout << "				i i i i" << endl;
+	cout << "			     __i_i_i_i_i__" << endl;
+	cout << "			    |" << "<3" << "         " << "<3" << "|" <<endl;
+	cout << "	            	  __|" << "  ÖÐRETMENLER"  << "|__" <<endl;
+	cout << "			 |" << "<3" << "   " << "<3"<< "   " << "<3" << "   " << "<3" << "  |" <<endl;
+	cout << "		       __|" << "<3 <3 GÜNÜNÜZ" << " " << "<3 <3" << "|__"<<endl;
+	cout << "		      |<3 " << "<3" << "   " << "<3"<< "   " << "<3" << "   " << "<3" << "   <3|" <<endl;
+	cout << "		      |" << "<3 <3 <3 KUTLU" << " " << "  <3 <3 <3" << "|"<<endl;
+	cout << "		      |<3 " << "<3" << "   " << "<3"<< "   " << "<3" << "   " << "<3" << "   <3|" <<endl;
+	cout << "		      |" << "<3 <3 <3   OLSUN" << " <3 <3 <3" << "|"<<endl;
+	cout << "		      |_________________________|"<<endl;
+	cout <<endl << endl << endl<< endl<< endl<< endl<< endl;
+
+	return 0;
+}
+
